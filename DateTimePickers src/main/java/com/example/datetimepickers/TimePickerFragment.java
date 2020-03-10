52dc3cbf614a1bfc77ed25a83a2a8dd608767448
Natjoe <57070788+Natjoe@users.noreply.github.com>
2020-03-09 23:38:57 -0500

    Add files via upload

---
+++ DateTimePickers src/main/java/com/example/datetimepickers/TimePickerFragment.java
@@ -0,0 +1,38 @@
+package com.example.datetimepickers;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import android.app.TimePickerDialog;
+import android.text.format.DateFormat;
+import android.widget.TimePicker;
+import java.util.Calendar;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processTimePickerResult(hourOfDay, minute);
+    }
+}
